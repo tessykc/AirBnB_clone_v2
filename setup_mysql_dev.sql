a8b754ce51544df4d5bbabdf4fd87294beb2f37f
jeshi2 <jamesvean19@gmail.com>
2023-08-20 23:28:42 +0300

    mid

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,6 +5,6 @@
 --hbnb_dev have select priviledge on db performance_schema
 --script won't fail if hbnb_dev or hbnb_dev_db
 CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BYhbnb_dev_pwd';
+CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY hbnb_dev_pwd';
 GRANT ALL PRIVILEDGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
