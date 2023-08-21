b68bc1bbcf2b7cd44bbbc67d293fe754cc79ac95
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-08-21 03:40:52 -0700

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     name = ""
+    __tablename__ = 'states'
+    name = Column(String (128), nullable=False)
+    for DBStorage:
+        cities = relationship('City', cascade = 'all, delete', backref = 'state')
+    for FileStorage:
+        getattr cities()
+        return (City)
+    City.state_id = State.id
+    FileStorage = relationship('State', 'City')
