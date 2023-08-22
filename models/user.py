63475447cf7faaf6be162a7604b549e0cbe32081
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-08-22 11:26:07 -0700

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
-from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR, DateTine
-from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from models.place import Place
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
