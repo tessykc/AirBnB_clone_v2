139d8d1286e5921ce4899b40e35708b3b4944649
jeshi2 <jamesvean19@gmail.com>
2023-08-23 17:41:52 +0300

    mid

--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BseaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
 from sqlalchemy.orm import relationship
 from models.city import City
