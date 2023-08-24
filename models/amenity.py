76824e46ba54318edfd8e48449372b7158691fbd
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-08-24 01:04:13 -0700

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+association_table = Table('association', Base.metadata,
+    Column('Place', ForeignKey('left.id')),
+    Column('Amenity', ForeignKey('right.id'))
+)
+
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place",
+                    secondary=association_table)
 
 
-class Amenity(BaseModel):
-    name = ""
+
+
