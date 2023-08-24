8663f380f79480d760b03fbd9d5d3a4e62b89111
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-08-24 02:30:54 -0700

    Update place.py

--- models/place.py
+++ models/place.py
@@ -4,6 +4,16 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import *
+from os import environ as env
+
+metadata = Base.metadata
+
+place_amenity = Table('place_amenity', metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True),
+    Column('amenity_id', String(60), ForeignKey('places.id'), primary_key=True)
+                     
+)
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -19,3 +29,19 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
+
+    def amenities(self):
+        """ relationship with the class Amenity """
+        if env.get('HBNB_TYPE_STORAGE') == 'db':
+            amenities = relationshiprelationship("Amenity",
+                    secondary=place_amenity, viewonly=False)
+
+        """ getting amenities within the place id """
+        ls = [
+            v for k, v in model.storage.all(model.Amenity).items()
+              if v.id in self.amenity_ids]
+        
+        return (ls)
+        for amenities in amenity_ids:
+            result.append(Amenity.id)
+        return result
