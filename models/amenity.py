bfbb6050cb25407a2516a22ea0325a30419ec0b2
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2023-08-24 02:45:13 -0700

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -5,16 +5,13 @@ from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-association_table = Table('association', Base.metadata,
-    Column('Place', ForeignKey('left.id')),
-    Column('Amenity', ForeignKey('right.id'))
-)
 
 class Amenity(BaseModel, Base):
+    """ This is the class for amenities. """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place",
-                    secondary=association_table)
+    place_amenities = relationship("Place", secondary=place_amenity,
+                                  backref="amenity")
 
 
 
