402bdf5bf2c576dab2abee62986f9116ff1bbded
jeshi2 <jamesvean19@gmail.com>
2023-08-26 23:43:20 +0300

    world

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""DBStorage module for HBNB project"""
+"""
+DBStorage module for HBNB project
+"""
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -11,13 +13,20 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class DBStorage:
-    """This class manages storage of hbnb models using SQLAlchemy"""
+    """
+    This class manages storage of hbnb models using SQLAlchemy
+    """
+
     __engine = None
     __session = None
 
     def __init__(self):
-        """Initialize DBStorage instance"""
+        """
+        Initialize DBStorage instance
+        """
+
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.
                                       format(getenv('HBNB_MYSQL_USER'),
                                              getenv('HBNB_MYSQL_PWD'),
@@ -29,7 +38,11 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query on the current database session all objects depending on class name"""
+        """
+        Query on the current database session 
+        all objects depending on class name
+        """
+
         from models import classes
         result = {}
         if cls:
@@ -44,20 +57,29 @@ class DBStorage:
         return result
 
     def new(self, obj):
-        """Add the object to the current database session"""
+        """
+        Add the object to the current database session
+        """
         self.__session.add(obj)
 
     def save(self):
-        """Commit all changes of the current database session"""
+        """
+        Commit all changes of the current database session
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete obj from current database session"""
+        """
+        Delete obj from current database session
+        """
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables in the database and create the current database session"""
+        """
+        Create all tables in the database and 
+        create the current database session
+        """
         Base.metadata.create_all(self.__engine)
         Session = scoped_session(sessionmaker(bind=self.__engine,
                                               expire_on_commit=False))
