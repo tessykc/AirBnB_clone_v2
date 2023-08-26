c0bbba3fb0b5953b7c0064d135d05e01d9f84e73
jeshi2 <jamesvean19@gmail.com>
2023-08-27 02:00:59 +0300

    world

--- models/user.py
+++ models/user.py
@@ -2,8 +2,12 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.review import Review
+from models.place import Place
 
-class User(BaseModel):
+
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
@@ -11,3 +15,5 @@ class User(BaseModel):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
