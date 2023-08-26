3eb2611b7a8711c4493f822487344c5dfbbdddfe
jeshi2 <jamesvean19@gmail.com>
2023-08-27 01:58:14 +0300

    world

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -84,4 +84,4 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         Session = scoped_session(sessionmaker(bind=self.__engine,
                                               expire_on_commit=False))
-        self.__session = Session()
+        self.__session = Session()  
