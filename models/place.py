37d4a646766bab24a4379575f7e8467074c6af33
jeshi2 <jamesvean19@gmail.com>
2023-08-27 02:26:43 +0300

    world

--- models/place.py
+++ models/place.py
@@ -18,3 +18,5 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+
+    reviews = relationship('Review', cascade='all, delete', backref='place')
