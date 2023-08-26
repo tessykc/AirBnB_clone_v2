88071a253b6548d9f0e75573b0340c21c70d6d7d
jeshi2 <jamesvean19@gmail.com>
2023-08-26 22:28:52 +0300

    mid

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Table, Column, Integer, ForeignKey, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from models.place import place_amenity
 
-association_table = Table('association', Base.metadata,
-    Column('Place', ForeignKey('left.id')),
-    Column('Amenity', ForeignKey('right.id'))
-)
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place",
                     secondary=association_table)
-
-
-
-
