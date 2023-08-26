3080555bb1406a135e063535522ffafcb32be4a9
jeshi2 <jamesvean19@gmail.com>
2023-08-27 01:10:00 +0300

    world

--- models/state.py
+++ models/state.py
@@ -2,9 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
-from sqlalchemy.orm import relationship, backref
-import os
-import models
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
@@ -12,18 +10,15 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     
-    #cities = relationship('City', cascade='all, delete', backref='state')
-    if os.getenv("HBNB_TYPE_STORAGE") != "db": 
+    if models.storage_type == 'db':
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
         @property
         def cities(self):
             """Returns the list of City instances with state_id equals to the current State.id"""
+            from models import storage
             city_list = []
-            city_dict = models.storage.all(models.city.City)
-            for key, value in city_dict.items():
+            for city in storage.all('City').values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
-
-    else:
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
