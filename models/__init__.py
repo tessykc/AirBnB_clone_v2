9473591d79ef9a707bc3e6c74309ae2cc340990c
jeshi2 <jamesvean19@gmail.com>
2023-08-27 10:22:41 +0300

    mid

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-
+from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
