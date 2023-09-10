e7ad7613f1bcaacf4b609e7c59e1deae8c7bdb94
jeshi2 <jamesvean19@gmail.com>
2023-09-11 00:00:55 +0300

    static_web

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive web_static
+"""
+
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """
+    compress file
+    """
+    now = datetime.utcnow()
+    archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+        now.year, now.month, now.day, now.hour, now.minute, now.second)
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/{} web_static".format(archive_name))
+    if result.succeeded:
+        return "versions/{}".format(archive_name)
+    else:
+        return None
+
+if __name__ == "__main__":
+    archive_path = do_pack()
+    if archive_path:
+        print("web_static packed: {}".format(archive_path))
+    else:
+        print("Packaging failed.")
