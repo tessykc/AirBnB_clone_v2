18b03596880c0c4736d5d633fe7255d57976f132
jeshi2 <jamesvean19@gmail.com>
2023-09-11 18:04:42 +0300

    static_web

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ do_deploy
 """
 
 from fabric.api import *
-import os
+import os.path import exists
 
 env.hosts = ['54.89.117.155', '34.229.137.26']
 """env.user = 'ubuntu'
@@ -17,32 +17,20 @@ def do_deploy(archive_path):
     """
     distibute archive to web server and deploy
     """
-    if not os.path.exists(archive_path):
+    if exists(archive_path) is False:
         return False
-    archive_name = os.path.basename(archive_path)
-    archive_no_ext = archive_name.replace('.tgz', '')
-
     try:
-        """upload to /tmp/ on server"""
-        put(archive_path, '/tmp/{}'.format(archive_name), use_sudo=True)
-
-        """create release dir"""
-        run('mkdir -p /data/web_static/releases/{}'
-            .format(archive_no_ext))
-
-        """decompress archive into release dir"""
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
-            .format(archive_name, archive_no_ext))
-
-        """del archive from server"""
-        run('rm /tmp/{}'.format(archive_name))
-
-        run('mv /data/web_static/releases/{}/web_static/*'
-            '/data/web_static/releases/{}/'
-            .format(archive_no_ext, archive_no_ext))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(archive_no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
 
         print("New version deployed!")
         return True
