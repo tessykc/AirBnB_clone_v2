9686ff7ffdbf8d7f44a04057162b5ede06bf32eb
jeshi2 <jamesvean19@gmail.com>
2023-09-12 15:54:24 +0300

    static_webs

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,8 +9,8 @@ from fabric.api import put, run, env
 from os.path import exists
 
 env.hosts = ['54.89.117.155', '34.229.137.26']
-"""env.user = 'ubuntu'
-env.key_filename = "~/.ssh/id_rsa"""
+env.user = 'ubuntu'
+env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
@@ -31,7 +31,8 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-
+        
+        print("New version deployed!")
         return True
     except:
         return False
