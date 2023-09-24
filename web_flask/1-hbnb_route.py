a86fab3092e818f2fd872dbb94bf7c3b27ac761b
jeshi2 <jamesvean19@gmail.com>
2023-09-24 12:55:25 +0300

    flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,9 +1,9 @@
 #!/usr/binpython3
 """
- script that starts a Flask web application
+script that starts a Flask web application
 """
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
 
 
